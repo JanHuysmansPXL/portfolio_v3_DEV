b02017b729f4ad8754453b0cf5c2fb060533ad3b
JanHuysmans <12201074@student.pxl.be>
2024-08-14 15:03:51 +0200

    Editet image exports on projects - via public path.

--- portfolio_v3/src/views/ProjectsView.vue
+++ portfolio_v3/src/views/ProjectsView.vue
@@ -1,20 +1,28 @@
+<template>
+  <div id="pagewrapper">
+    <NavbarComponent />
+
+    <main class="maintest pt-lg">
+      <section class="mt-md">
+        <div class="projects-grid-gallery mt-sm">
+          <router-link to="/" class="projects-grid-card" v-for="(project, index) in projects" :key="index">
+            <img :src="project.image" class="projects-grid-card-img" :alt="project.title" />
+            <div class="projects-grid-labelrow">
+              <p class="projects-grid-title">{{ project.title }}</p>
+              <p class="projects-grid-subtitle txt-ss-body1 txt-nt-100 mt-xs txt-width-90">{{ project.description }}</p>
+            </div>
+          </router-link>
+        </div>
+      </section>
+
+      <FooterComponent />
+    </main>
+  </div>
+</template>
 
 <script>
 import NavbarComponent from "@/components/NavbarComponent.vue";
 import FooterComponent from "@/components/FooterComponent.vue";
-
-// Import your images
-import coverImage from '@/public/images/covers/coverimage.png';
-import coverImage1 from '@/public/images/covers/coverimage-1.png';
-import coverImage2 from '@/public/images/covers/coverimage-2.png';
-import coverImage3 from '@/public/images/covers/coverimage-3.png';
-import coverImage4 from '@/public/images/covers/coverimage-4.png';
-import coverImage5 from '@/public/images/covers/coverimage-5.png';
-import coverImage6 from '@/public/images/covers/coverimage-6.png';
-import coverImage7 from '@/public/images/covers/coverimage-7.png';
-import coverImage8 from '@/public/images/covers/coverimage-8.png';
-import coverImage9 from '@/public/images/covers/coverimage-9.png';
-
 
 export default {
   data() {
@@ -22,63 +30,63 @@ export default {
       projects: [
         {
           title: "Made Digital Brand Book",
-          image: coverImage,
+          image: '/assets/images/covers/coverimage.png',
           icon: null,
           description: "How I defined, designed and delivered an interactive online brand guide for the new Made brand identity.",
         },
         {
-        title: "Kronolith",
-        image: coverImage1,          
-        icon: "../src/assets/images/square-dont.png",
-        description: "Design and 3D-animation of a wall-mounted clock.",
+          title: "Kronolith",
+          image: '/assets/images/covers/coverimage-1.png',
+          icon: '/assets/images/square-dont.png',
+          description: "Design and 3D-animation of a wall-mounted clock.",
         },
         {
           title: "Buildr",
-          image: coverImage2,           
+          image: '/assets/images/covers/coverimage-2.png',
           icon: null,
-          description: "Guiding a group of young designers to design and develop an accesible job matching platform.",
+          description: "Guiding a group of young designers to design and develop an accessible job matching platform.",
         },
         {
-          title: "Auro",
-          image: coverImage3,           
+          title: "Made Digital Brand Book",
+          image: '/assets/images/covers/coverimage-3.png',
           icon: null,
-          description: "Webdesign and development for an imaginative wearables brand, powered by Midjourney.",
+          description: "How I defined, designed and delivered an interactive online brand guide for the new Made brand identity.",
         },
         {
-          title: "Tiny House",
-          image: coverImage4,           
-          icon: null,
-          description: "Branding a joyful brand for a compact lifestyle.",
+          title: "Kronolith",
+          image: '/assets/images/covers/coverimage-4.png',
+          icon: '/assets/images/square-dont.png',
+          description: "Design and 3D-animation of a wall-mounted clock.",
         },
         {
-          title: "Holy Cow!",
-          image: coverImage5,           
+          title: "Buildr",
+          image: '/assets/images/covers/coverimage-5.png',
           icon: null,
-          description: "How I designed an offbeat burger chain brand and app.",
+          description: "Guiding a group of young designers to design and develop an accessible job matching platform.",
         },
         {
-          title: "Transit",
-          image: coverImage6,           
+          title: "Made Digital Brand Book",
+          image: '/assets/images/covers/coverimage-6.png',
           icon: null,
-          description: "3D-sculptures for Transit, a techno/house-event with headliners like Mall Grab.",
+          description: "How I defined, designed and delivered an interactive online brand guide for the new Made brand identity.",
         },
         {
-          title: "Onyx",
-          image: coverImage7,           
-          icon: null,
-          description: "A handmade font that rocks.",
+          title: "Kronolith",
+          image: '/assets/images/covers/coverimage-7.png',
+          icon: '/assets/images/square-dont.png',
+          description: "Design and 3D-animation of a wall-mounted clock.",
         },
         {
-          title: "Collider",
-          image: coverImage8,           
+          title: "Buildr",
+          image: '/assets/images/covers/coverimage-8.png',
           icon: null,
-          description: "A collection of handmade drawings and Axidraw prints.",
+          description: "Guiding a group of young designers to design and develop an accessible job matching platform.",
         },
         {
-          title: "Line Drawings",
-          image: coverImage9,           
+          title: "Buildr",
+          image: '/assets/images/covers/coverimage-9.png',
           icon: null,
-          description: "A collection of intricate ballpoint drawings and colour studies.",
+          description: "Guiding a group of young designers to design and develop an accessible job matching platform.",
         },
         // Add more projects as needed...
       ],
@@ -90,54 +98,3 @@ export default {
   },
 };
 </script>
-
-<template>
-  <div id="pagewrapper">
-    <!-- NAVBAR WITH MENU -->
-    <NavbarComponent />
-
-    <!-- MAIN BODY -->
-    <main class="maintest pt-lg">
-      <section class="mt-md">
-
-
-        <div class="projects-head ms-md">
-            <div class="projects-bar-top">
-                <h4 class="txt-ss-h3 txt-width-60">Projects</h4>
-            </div>
-            <div class="projects-bar-bottom">
-                <ul class="projects-filter-list">
-                    <li><a href="#" class="txt-ss-sub2 filter-link">All</a></li>
-                    <li><a href="#" class="txt-ss-sub2  filter-link">Digital</a></li>
-                    <li><a href="#" class="txt-ss-sub2  filter-link">Branding</a></li>
-                    <li><a href="#" class="txt-ss-sub2  filter-link">Art</a></li>
-                </ul>
-                <button class="view-button-box">
-                    <img class="link-arrow" src="../assets/arrow-right-link.png" alt="arrow">
-                    <a class="link-text" href="#">View List</a>
-                </button>
-            </div>
-            <div class="divider"></div>
-         
-        </div>
-
-
-        <!-- GRID SECTION - PROJECTS GALLERY -->
-        <div class="projects-grid-gallery mt-sm">
-          <router-link to="/" class="projects-grid-card" v-for="(project, index) in projects" :key="index">
-            <img :src="project.image" class="projects-grid-card-img" :alt="project.title" />
-            <div class="projects-grid-labelrow">
-             <!-- <img v-if="project.icon" :src="project.icon" class="projects-grid-labelicon label-divider" /> -->
-              <p class="projects-grid-title">{{ project.title }}</p>
-            <p class="projects-grid-subtitle txt-ss-body1 txt-nt-100 mt-xs txt-width-90">{{ project.description }}</p>
-            </div>
-          </router-link>
-        </div>
-
-      </section>
-
-      <!-- FOOTER -->
-      <FooterComponent />
-    </main>
-  </div>
-</template>
