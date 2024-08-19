8f34d578632e04662b7b718b29827b21aa3ca14a
JanHuysmans <12201074@student.pxl.be>
2024-08-19 23:32:30 +0200

    Added tinyHouse content and brand guide slides.

---
+++ portfolio_v3/src/components/TinyHouseBrandSlides.vue
@@ -0,0 +1,166 @@
+<template>
+    <div class="carousel-wrapper">
+      <div class="carousel-container">
+        <div class="carousel">
+          <div
+            v-for="(image, index) in images"
+            :key="index"
+            :class="['carousel-item', { active: currentSlide === index }]"
+          >
+            <img :src="image.src" :alt="image.alt" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="scroll-buttons">
+          <button class="scroll-button prev" @click="nextSlide">
+            <img class="scroll-arrow" src="../assets/arrow-right-link.png" alt="arrow">
+          </button>
+          <button class="scroll-button next" @click="prevSlide">
+            <img class="scroll-arrow" src="../assets/arrow-right-link.png" alt="arrow">
+          </button>
+    </div>
+
+          <!-- CTA BUTTON -->
+    <div class="cta-wrapper container-fluid">
+        <div class="cta-buttonbar">
+          <button class="cta-button">
+            <img class="link-arrow" src="../assets/arrow-right-link.png" alt="arrow">
+            <a href="/assets/images/projects/tinyhouse/tinyhouse_brandbook.pdf" target="_blank" type="download" class="link-text">{{ buttonWebsiteText }}</a>
+          </button>
+        </div>
+    </div>
+
+
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        buttonWebsiteText: "Download the Brandguide PDF",
+        currentSlide: 0,
+        images: [
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_01.jpg", alt: "TinyHouse Brandbook Page 1" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_02.jpg", alt: "TinyHouse Brandbook Page 2" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_03.jpg", alt: "TinyHouse Brandbook Page 3" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_04.jpg", alt: "TinyHouse Brandbook Page 4" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_05.jpg", alt: "TinyHouse Brandbook Page 5" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_06.jpg", alt: "TinyHouse Brandbook Page 6" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_07.jpg", alt: "TinyHouse Brandbook Page 7" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_08.jpg", alt: "TinyHouse Brandbook Page 8" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_09.jpg", alt: "TinyHouse Brandbook Page 9" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_10.jpg", alt: "TinyHouse Brandbook Page 10" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_11.jpg", alt: "TinyHouse Brandbook Page 11" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_12.jpg", alt: "TinyHouse Brandbook Page 12" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_13.jpg", alt: "TinyHouse Brandbook Page 13" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_14.jpg", alt: "TinyHouse Brandbook Page 14" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_16.jpg", alt: "TinyHouse Brandbook Page 16" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_17.jpg", alt: "TinyHouse Brandbook Page 17" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_18.jpg", alt: "TinyHouse Brandbook Page 18" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_19.jpg", alt: "TinyHouse Brandbook Page 19" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_20.jpg", alt: "TinyHouse Brandbook Page 20" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_21.jpg", alt: "TinyHouse Brandbook Page 21" },
+          { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_22.jpg", alt: "TinyHouse Brandbook Page 22" },
+
+        ],
+      };
+    },
+    methods: {
+      nextSlide() {
+        this.currentSlide = (this.currentSlide + 1) % this.images.length;
+      },
+      prevSlide() {
+        this.currentSlide =
+          (this.currentSlide - 1 + this.images.length) % this.images.length;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .carousel-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: 0 32px;
+    width: 100%;
+    height: auto;
+  }
+  
+  .carousel-container {
+    position: relative;
+    width: 100%;
+    max-width: 1440px;
+    margin-bottom: 16px;
+  }
+  
+  .carousel {
+    overflow: hidden;
+    position: relative;
+    width: 100%;
+    height: auto;
+    max-height: 720px;
+    border-radius: 8px;
+  }
+  
+  .carousel-item {
+    opacity: 0;
+    transition: opacity 0.5s ease-in-out;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  
+  .carousel-item.active {
+    opacity: 1;
+    position: relative;
+  }
+  
+  .carousel-item img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain; /* Ensures the image is fully visible */
+    border-radius: 8px;
+  }
+  
+  .carousel-controls {
+    display: flex;
+    justify-content: center;
+    margin-top: 16px;
+  }
+  
+  .carousel-control-prev,
+  .carousel-control-next {
+    background: none;
+    border: none;
+    font-size: 24px;
+    cursor: pointer;
+    padding: 8px 12px;
+  }
+  
+  .carousel-control-prev i,
+  .carousel-control-next i {
+    font-size: 24px;
+    color: #000;
+  }
+  
+  .download-link-container {
+    text-align: center;
+    margin-top: 20px;
+  }
+  
+  .download-link {
+    font-weight: bold;
+    color: #333;
+    text-decoration: none;
+  }
+  
+  .download-link:hover {
+    text-decoration: underline;
+  }
+  </style>
--- portfolio_v3/src/views/TinyhouseView.vue
+++ portfolio_v3/src/views/TinyhouseView.vue
@@ -2,6 +2,7 @@
 import FooterComponent from "@/components/FooterComponent.vue";
 import MarqueeBar from "@/components/MarqueeBar.vue";
 import transitVideoImport from '/assets/images/projects/tinyhouse/TinyHouse - Instagram Posts/TinyHouse---Instagram-Story-1080x1920-n2.gif';
+import TinyHouseBrandSlides from '@/components/TinyHouseBrandSlides.vue';
 
 
 export default {
@@ -14,6 +15,7 @@ export default {
   components: {
     FooterComponent,
     MarqueeBar,
+    TinyHouseBrandSlides,
   },
   methods: {
     scrollPrev() {
@@ -30,18 +32,14 @@ export default {
   <div id="pagewrapper">
     <main class="maintest">
 
-
       <!-- FULL IMAGE  -->
- <!-- FULL IMAGE  -->
-       <div class="container-fluid">
-        <img class="img-fluid project-heading-img" src="/assets/images/projects/tinyhouse/tinyhouse-cover-1.png
-"
+      <div class="container-fluid">
+        <img class="img-fluid project-heading-img" src="/assets/images/projects/tinyhouse/tinyhouse-cover-1.png"
           alt="An image of the made icon graphic language">
       </div>
-       
 
       <!-- TITLE BLOCK  -  1. WORDMARK -->
-      <section id="AuroHead" class="project-title-block bg-nt-10">
+      <section class="project-title-block bg-nt-10">
         <h2 class="project-title-block-title">Tiny House</h2>
         <!-- INFO SECTION -->
         <div class="project-info-block mt-md">
@@ -50,7 +48,9 @@ export default {
           </div>
           <div class="intro-block">
             <h6 class="intro-title txt-ss-h6">Inspiring people to live smaller trough joyful branding.</h6>
-            <p class="intro-subtitle txt-ss-body1 txt-pp-80">TinyHouse is an ecological brand that brings affordable and durable tiny houses to the masses. For this project I made the logo and brand guide, online banners and Instagram content.</p>
+            <p class="intro-subtitle txt-ss-body1 txt-nt-60">TinyHouse is an ecological brand that brings affordable and
+              durable tiny houses to the masses. For this project I made the logo and brand guide, online banners and
+              Instagram content.</p>
           </div>
           <div class="details-block">
             <h5 class="details-title txt-ss-sub2">Branding / SOCIAL MARKETING</h5>
@@ -93,18 +93,30 @@ export default {
         <h4 class="quote-block-text-half txt-ss-h4 txt-nt-100">
           Living smaller and greener.
         </h4>
-        <p class="quote-block-text-half txt-ss-body1 txt-nt-100 mt-sm">TinyHouse is about inspiring people to live smaller and greener. For this brand I wanted a fresh and youthful image. I developed the logo, brand guide and social content for the brand.</p>
+        <p class="quote-block-text-half txt-ss-body1 txt-nt-100 mt-sm">TinyHouse is about inspiring people to live
+          smaller and greener. For this brand I wanted a fresh and youthful image. I developed the logo, brand guide and
+          social content for the brand.</p>
       </section>
 
       <!-- CAROUSEL SLIDER FOR INSTA SLIDES GRID  -->
 
       <div class="container-fluid py-xl bg-nt-10">
         <div class="grid-images-wrapper" ref="imageWrapper">
-          <div class="grid-box"><img class="grid-image post" src="/assets/images/projects/tinyhouse/TinyHouse - Instagram Posts/TinyHouse---Instagram-PostsPOST-1.png" alt=""></div>
-          <div class="grid-box"><img class="grid-image post" src="/assets/images/projects/tinyhouse/TinyHouse - Instagram Posts/TinyHouse---Instagram-PostsPOST-2.png" alt=""></div>
-          <div class="grid-box"><img class="grid-image post" src="/assets/images/projects/tinyhouse/TinyHouse - Instagram Posts/TinyHouse---Instagram-PostsPOST-3.png" alt=""></div>
-          <div class="grid-box"><img class="grid-image post" src="/assets/images/projects/tinyhouse/TinyHouse - Instagram Posts/TinyHouse---Instagram-PostsPOST-4.png" alt=""></div>
-          <div class="grid-box"><img class="grid-image post" src="/assets/images/projects/tinyhouse/TinyHouse - Instagram Posts/TinyHouse---Instagram-PostsPOST-5.png" alt=""></div>
+          <div class="grid-box"><img class="grid-image post"
+              src="/assets/images/projects/tinyhouse/TinyHouse - Instagram Posts/TinyHouse---Instagram-PostsPOST-1.png"
+              alt=""></div>
+          <div class="grid-box"><img class="grid-image post"
+              src="/assets/images/projects/tinyhouse/TinyHouse - Instagram Posts/TinyHouse---Instagram-PostsPOST-2.png"
+              alt=""></div>
+          <div class="grid-box"><img class="grid-image post"
+              src="/assets/images/projects/tinyhouse/TinyHouse - Instagram Posts/TinyHouse---Instagram-PostsPOST-3.png"
+              alt=""></div>
+          <div class="grid-box"><img class="grid-image post"
+              src="/assets/images/projects/tinyhouse/TinyHouse - Instagram Posts/TinyHouse---Instagram-PostsPOST-4.png"
+              alt=""></div>
+          <div class="grid-box"><img class="grid-image post"
+              src="/assets/images/projects/tinyhouse/TinyHouse - Instagram Posts/TinyHouse---Instagram-PostsPOST-5.png"
+              alt=""></div>
         </div>
         <div class="scroll-buttons mt-sm">
           <button class="scroll-button prev" @click="scrollPrev">
@@ -124,40 +136,44 @@ export default {
       </div>  -->
 
       <!-- Heading Block - II -->
-      <section class="quote-block bg-nt-100 pb-lg">
-        <h4 class="quote-block-text-half txt-ss-h4 txt-nt-10">
-          I used Midjourney to generate a consistent line of products that reflect the Auro philosophy.
-        </h4>
-        <p class="quote-block-text-half txt-ss-body1 txt-nt-10 mt-sm">Midjourney is an AI-tool to generate creative
-          images. To visualise the Auro products I studied the use of advanced Midjourney settings. Experiments with
-          prompting helped met to further align a total of 20 Auro products.</p>
-      </section>
-
-      <!-- FULL VIDEO  -->
-      <div class="container-fluid bg-white">
-        <video class="vid-pagewidth padded" autoplay loop>
-          <source :src="vidOne" type="video/mp4"> Your browser does not support the video tag.
-        </video>
-      </div>
-
-      <!-- Info Block - WEBSITE -->
-      <section class="quote-block bg-white">
+      <section class="quote-block bg-white py-lg">
         <h4 class="quote-block-text-half txt-ss-h4 txt-nt-100">
-          To develop the website I used Craft CMS, Vue and Wordpress.
-        </h4>
-        <p class="quote-block-text-half txt-ss-body1 txt-nt-100 mt-sm">This website was made as an exercise mostly. I developed
-          the website three times. Once in Vue, another time with Wordpress and a third time with Craft CMS.</p>
+          Instagram posts and Google banners. I wrote the copy and made the designs as simple as possible. </h4>
+        <p class="quote-block-text-half txt-ss-body1 txt-pp-80 mt-sm">Figma ipsum component variant main layer. Layer
+          stroke prototype comment share invite strikethrough. Figma fill opacity plugin vector fill. Link image inspect
+          share draft group.</p>
       </section>
 
-      <!-- CTA BUTTONS -->
-      <div class="cta-wrapper container-fluid">
-        <div class="cta-buttonbar">
-          <button class="cta-button">
-            <img class="link-arrow" src="../assets/arrow-right-link.png" alt="arrow">
-            <a href="https://auro-future.com/" target="_blank" class="link-text">{{ buttonWebsiteText }}</a>
-          </button>
-        </div>
+      <!-- FULL IMAGE -- FULL SCREEN WIDTH -->
+      <div class="container-fluid">
+        <img class="img-fluid project-heading-img auto"
+          src="/assets/images/projects/tinyhouse/Tinyhouse - Mockups/Small Cargo Truck Mockup.jpg"
+          alt="An image of the made icon graphic language">
       </div>
+
+      <!-- Heading Block - II -->
+      <section class="quote-block bg-white py-lg">
+        <h4 class="quote-block-text-half txt-ss-h4 txt-nt-100">
+          Tiny House has a dedicated brandguide where the identity of the brand is elaborated. </h4>
+        <p class="quote-block-text-half txt-ss-body1 txt-pp-80 mt-sm">Have a look at the finished brand guide here.</p>
+      </section>
+
+      <!-- BRAND SLIDES-->
+
+      <TinyHouseBrandSlides/>
+
+      <!-- FULL IMAGE -- FULL SCREEN WIDTH -->
+      <div class="container-fluid mt-lg">
+        <img class="img-fluid project-heading-img auto"
+          src="/assets/images/projects/tinyhouse/tinyhouse-mockup-banner.png" alt="An image of some tinyhouse mockups">
+      </div>
+      <!-- QUOTE BLOCK "" -->
+      <section class="container-fluid bg-nt-10">
+        <div class="quote-block-wrapper">
+          <h6 class="txt-ss-h5 txt-gr-80">“I got a free student upgrade after I needed over 500 elements to place on my
+            boards. — Thank you, Whimsical.”</h6>
+        </div>
+      </section>
 
       <div class="container-fluid mockup-center bg-nt-10 mt-md">
         <img class="mockup-image" src="/assets/images/projects/auro/auro-mockup-mobile-1.png" alt="arrow">
@@ -172,11 +188,11 @@ export default {
         </router-link>
       </div>
 
-        <!-- MARQUEE BAR -->
-        <MarqueeBar />
+      <!-- MARQUEE BAR -->
+      <MarqueeBar />
 
-        <!-- FOOTER -->
-        <FooterComponent />
+      <!-- FOOTER -->
+      <FooterComponent />
 
     </main>
   </div>
