f95177618e34e20f4a52960307e5d3ae9ad08066
JanHuysmans <12201074@student.pxl.be>
2024-08-19 23:46:28 +0200

    Edit on TH carousel height.

--- portfolio_v3/src/components/TinyHouseBrandSlides.vue
+++ portfolio_v3/src/components/TinyHouseBrandSlides.vue
@@ -1,47 +1,44 @@
 <template>
-    <div class="carousel-wrapper">
-      <div class="carousel-container">
-        <div class="carousel">
-          <div
-            v-for="(image, index) in images"
-            :key="index"
-            :class="['carousel-item', { active: currentSlide === index }]"
-          >
-            <img :src="image.src" :alt="image.alt" />
-          </div>
+  <div class="carousel-wrapper">
+    <div class="carousel-container">
+      <div class="carousel">
+        <div
+          v-for="(image, index) in images"
+          :key="index"
+          :class="['carousel-item', { active: currentSlide === index }]"
+        >
+          <img :src="image.src" :alt="image.alt" />
         </div>
       </div>
     </div>
-
-    <div class="scroll-buttons">
-          <button class="scroll-button prev" @click="nextSlide">
-            <img class="scroll-arrow" src="../assets/arrow-right-link.png" alt="arrow">
-          </button>
-          <button class="scroll-button next" @click="prevSlide">
-            <img class="scroll-arrow" src="../assets/arrow-right-link.png" alt="arrow">
-          </button>
+  </div>
+  <div class="scroll-buttons">
+      <button class="scroll-button prev" @click="prevSlide">
+        <img class="scroll-arrow" src="../assets/arrow-right-link.png" alt="arrow">
+      </button>
+      <button class="scroll-button next" @click="nextSlide">
+        <img class="scroll-arrow" src="../assets/arrow-right-link.png" alt="arrow">
+      </button>
     </div>
 
-          <!-- CTA BUTTON -->
+    <!-- CTA BUTTON -->
     <div class="cta-wrapper container-fluid">
-        <div class="cta-buttonbar">
-          <button class="cta-button">
-            <img class="link-arrow" src="../assets/arrow-right-link.png" alt="arrow">
-            <a href="/assets/images/projects/tinyhouse/tinyhouse_brandbook.pdf" target="_blank" type="download" class="link-text">{{ buttonWebsiteText }}</a>
-          </button>
-        </div>
+      <div class="cta-buttonbar">
+        <button class="cta-button">
+          <img class="link-arrow" src="../assets/arrow-right-link.png" alt="arrow">
+          <a href="/assets/images/projects/tinyhouse/tinyhouse_brandbook.pdf" target="_blank" type="download" class="link-text">{{ buttonWebsiteText }}</a>
+        </button>
+      </div>
     </div>
+</template>
 
-
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        buttonWebsiteText: "Download the Brandguide PDF",
-        currentSlide: 0,
-        images: [
+<script>
+export default {
+  data() {
+    return {
+      buttonWebsiteText: "Download the Brandguide PDF",
+      currentSlide: 0,
+      images: [
           { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_01.jpg", alt: "TinyHouse Brandbook Page 1" },
           { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_02.jpg", alt: "TinyHouse Brandbook Page 2" },
           { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_03.jpg", alt: "TinyHouse Brandbook Page 3" },
@@ -63,104 +60,69 @@
           { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_20.jpg", alt: "TinyHouse Brandbook Page 20" },
           { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_21.jpg", alt: "TinyHouse Brandbook Page 21" },
           { src: "/assets/images/projects/tinyhouse/tinyhouse_brandbook/tinyhouse_bb_22.jpg", alt: "TinyHouse Brandbook Page 22" },
+        // Add all other image objects here
+      ],
+    };
+  },
+  methods: {
+    nextSlide() {
+      this.currentSlide = (this.currentSlide + 1) % this.images.length;
+    },
+    prevSlide() {
+      this.currentSlide = (this.currentSlide - 1 + this.images.length) % this.images.length;
+    },
+  },
+};
+</script>
 
-        ],
-      };
-    },
-    methods: {
-      nextSlide() {
-        this.currentSlide = (this.currentSlide + 1) % this.images.length;
-      },
-      prevSlide() {
-        this.currentSlide =
-          (this.currentSlide - 1 + this.images.length) % this.images.length;
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .carousel-wrapper {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    padding: 0 32px;
-    width: 100%;
-    height: auto;
-  }
-  
-  .carousel-container {
-    position: relative;
-    width: 100%;
-    max-width: 1440px;
-    margin-bottom: 16px;
-  }
-  
-  .carousel {
-    overflow: hidden;
-    position: relative;
-    width: 100%;
-    height: auto;
-    max-height: 720px;
-    border-radius: 8px;
-  }
-  
-  .carousel-item {
-    opacity: 0;
-    transition: opacity 0.5s ease-in-out;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-  
-  .carousel-item.active {
-    opacity: 1;
-    position: relative;
-  }
-  
-  .carousel-item img {
-    width: 100%;
-    height: 100%;
-    object-fit: contain; /* Ensures the image is fully visible */
-    border-radius: 8px;
-  }
-  
-  .carousel-controls {
-    display: flex;
-    justify-content: center;
-    margin-top: 16px;
-  }
-  
-  .carousel-control-prev,
-  .carousel-control-next {
-    background: none;
-    border: none;
-    font-size: 24px;
-    cursor: pointer;
-    padding: 8px 12px;
-  }
-  
-  .carousel-control-prev i,
-  .carousel-control-next i {
-    font-size: 24px;
-    color: #000;
-  }
-  
-  .download-link-container {
-    text-align: center;
-    margin-top: 20px;
-  }
-  
-  .download-link {
-    font-weight: bold;
-    color: #333;
-    text-decoration: none;
-  }
-  
-  .download-link:hover {
-    text-decoration: underline;
-  }
-  </style>
+<style scoped>
+.carousel-wrapper {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 0 32px;
+  width: 100%;
+  height: auto;
+}
+
+.carousel-container {
+  position: relative;
+  width: 100%;
+  max-width: 1440px;
+  margin-bottom: 16px;
+}
+
+.carousel {
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+  height: auto;
+  border-radius: 8px;
+}
+
+.carousel-item {
+  opacity: 0;
+  transition: opacity 0.5s ease-in-out, transform 0.5s ease-in-out;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+
+.carousel-item.active {
+  opacity: 1;
+  transform: scale(1);
+  position: relative;
+  z-index: 2;
+}
+
+.carousel-item img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  border-radius: 8px;
+}
+</style>
