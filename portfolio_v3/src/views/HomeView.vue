a6ce6baa6efc2ad16c26362d8b7c85bd15162aad
JanHuysmans <12201074@student.pxl.be>
2024-10-08 00:58:11 +0200

    updated CTA text

--- portfolio_v3/src/views/HomeView.vue
+++ portfolio_v3/src/views/HomeView.vue
@@ -192,7 +192,8 @@ gsap.registerPlugin(ScrollTrigger);
                 <div class="cta-buttonbar">
                     <button class="cta-button">
                         <img class="link-arrow" src="/assets/arrow-right-link.png" alt="arrow">
- c                    </button>
+                        <router-link to="/work" class="link-text">{{ buttonWorkText }}</router-link>
+                    </button>
                     <button class="cta-button">
                         <img class="link-arrow" src="/assets/arrow-right-link.png" alt="arrow">
                         <a href="mailto:jan_huysmans@outlook.com" class="link-text">Get in touch</a>
